109a6beb2aceaa1b8e7393ed4ec3631313ff2384
Doggernaut-San <KirillSovmenOfficial@yandex.ru>
2024-10-17 00:51:26 +0500

    "Data Types and Variables" commit

---
+++ C#/Modules/Data Types and Variables/Galactic Travel Agency/Program.cs
@@ -0,0 +1,18 @@
+﻿string passengerName = "Zara";
+int passengerAge = 29;
+string ticketType = "First Class";
+string preferredPlanet = "Mars";
+
+Console.WriteLine(passengerName);
+Console.WriteLine(passengerAge);
+Console.WriteLine(ticketType);
+Console.WriteLine(preferredPlanet);
+
+double passengerAgeDouble = passengerAge;
+Console.WriteLine(passengerAgeDouble);
+
+double passengerAgeDouble2 = (double)passengerAge;
+Console.WriteLine(passengerAgeDouble2);
+
+string passengerAgeString = Convert.ToString(passengerAge);
+Console.WriteLine(passengerAgeString);
