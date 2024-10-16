0564af67791344996f91c6b901df8faa74681d57
Doggernaut-San <KirillSovmenOfficial@yandex.ru>
2024-10-16 17:11:06 +0500

    First commit

---
+++ C#/Modules/Hello World/Console Creatures/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine(" .-.");
+Console.WriteLine("(o o)");
+Console.WriteLine("| O |");
+Console.WriteLine("|   |");
+Console.WriteLine("'~~~'");
+
+Console.WriteLine("\nPedro\n");
+
+Console.WriteLine("c_    ____");
+Console.WriteLine("  \\\\__|__|__");
+Console.WriteLine("    \\( o_o)");
+Console.WriteLine("      > ~  >");
+Console.WriteLine("     /    / \\\\");
+Console.WriteLine("    /    /   \\\\");
+Console.WriteLine("    \\   )    c/");
+Console.WriteLine("    /  /");
+Console.WriteLine("   /  /|");
+Console.WriteLine("  (  ( \\");
+Console.WriteLine("  |  |  \\");
+Console.WriteLine("  | / \\  )");
+Console.WriteLine("  | | ) |");
+Console.WriteLine(" /  )(_/");
+Console.WriteLine("(_ /");
+
+Console.WriteLine("\nDancing Man\n");
+
+Console.WriteLine("    (    )");
+Console.WriteLine("  (  (  )  )");
+Console.WriteLine("(  (      )  )");
+Console.WriteLine("  (  (  )  )");
+Console.WriteLine("    (    )");
+Console.WriteLine("   /------\\");
+Console.WriteLine("  /  \\  /  \\");
+Console.WriteLine(" /    o o   \\");
+Console.WriteLine("/      o     \\");
+Console.WriteLine("**************");
+
+Console.WriteLine("\nAngry Volcano\n");
+
+Console.WriteLine("   /   \\");
+Console.WriteLine("   \\. ./");
+Console.WriteLine("  (o + o)");
+Console.WriteLine("--|  |  |--");
+Console.WriteLine("--|  |  |--");
+Console.WriteLine("--|  |  |--");
+Console.WriteLine("   v   v");
+Console.WriteLine("   *****");
+
+Console.WriteLine("\nSuper Bug\n");
